e1a73f3ab65b6e3aae1e818e6493f806a72b3017
C.J. O'Leary <cjoleary17@gmail.com>
2022-12-02 08:16:44 -0700

    mod 5 assignment 4

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -70,6 +70,7 @@ const ExpenseForm = (props) => {
             </div>
 
             <div className='new-expense__actions'>
+                <button type="cancel">Cancel</button>
                 <button type="submit">Add Expense</button>
             </div>
         </form>
