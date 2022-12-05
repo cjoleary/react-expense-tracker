015e261d21bac5cb93134e4ac6b294d6631f8cf5
C.J. O'Leary <cjoleary17@gmail.com>
2022-12-05 07:47:00 -0700

    sec 5 mod 68, 69

--- src/components/Expenses/ExpensesList.js
+++ src/components/Expenses/ExpensesList.js
@@ -3,7 +3,7 @@ import React from 'react';
 import ExpenseItem from './ExpenseItem';
 import './ExpensesList.css';
 
-const ExpensesList = props => {
+const ExpensesList = (props) => {
     if (props.items.length === 0) {
         return <h2 className='expenses-list__fallback'>Found no expenses.</h2>
     }
