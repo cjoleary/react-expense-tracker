651c619a618f3dfd64204fe5acab3470ba491397
C.J. O'Leary <cjoleary17@gmail.com>
2022-12-05 08:09:29 -0700

    sec 5 mod 70

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
 
-import ExpensesFilter from "./ExpensesFilter";
 import Card from '../UI/Card';
-import './Expenses.css';
+import ExpensesFilter from "./ExpensesFilter";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
+import './Expenses.css';
 
 const Expenses = (props) => {
     const [filteredYear, setFilteredYear] = useState('2022');
@@ -19,6 +20,7 @@ const Expenses = (props) => {
     return (
         <Card className='expenses'>
             <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
+            <ExpensesChart expenses={filteredExpenses}/>
             <ExpensesList items={filteredExpenses}/>
         </Card>
     );
